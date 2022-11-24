89240e80b6eb6b70ac0f4e12fb94e3f254487c31
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-24 16:02:02 +0530

    uc3 for addding the data

--- LinkedList.java
+++ LinkedList.java
@@ -4,10 +4,34 @@ public class LinkedList<T> {
     Node<T> head;
     Node<T> tail;
 
+    void push(T data){
+        Node<T> node = new Node<>(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        }
+        else{
+            node.next = head;
+            head = node;
+        }
+    }
+
+    void add(T data){
+        Node<T> node = new Node<>(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        }
+        else{
+            tail.next = node;
+            tail = node;
+        }
+    }
+
     void display(){
         Node<T> temp = head;
         while (temp != null){
-            System.out.println(temp.data);
+            System.out.print(temp.data+ " ");
             temp = temp.next;
         }
     }
