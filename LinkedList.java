76b314e9f006caab312adb5543919078dc45db89
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-24 16:04:07 +0530

    poping the element

--- LinkedList.java
+++ LinkedList.java
@@ -7,8 +7,57 @@ public class LinkedList<T> {
     void display(){
         Node<T> temp = head;
         while (temp != null){
-            System.out.println(temp.data);
+            Systpackage com.bridgelabz.linkedlist;
+
+public class LinkedList<T> {
+    Node<T> head;
+    Node<T> tail;
+
+    void push(T data){
+        Node<T> node = new Node<>(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        }
+        else{
+            node.next = head;
+            head = node;
+        }
+    }
+
+    void add(T data){
+        Node<T> node = new Node<>(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        }
+        else{
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    void display(){
+        Node<T> temp = head;
+        while (temp != null){
+            System.out.print(temp.data+ " ");
             temp = temp.next;
         }
     }
+
+    public void insert(T data) {
+        Node<T> node = new Node<>(data);
+        head.next = node;
+        node.next = tail;
+    }
+
+    public T pop() {
+        T deletedElement = head.data;
+        head = head.next;
+        return deletedElement;
+    }em.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+}
 }
